080b6183abc412a99eee55270b14ee18a25d907a
sanket <sanket.deshpande@caring-research.com>
2023-02-16 18:33:33 +0530

    updated config

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
-''' task retries on faliure '''
-TASK_RETRY_COUNT = 3
 
-
+# ---------------------------------------------------------
 #  state settings
-''' '''
+''' set redis connection '''
 STATE_MANAGER = 'REDIS'
 REDIS_CON = {
     "HOST" : '127.0.0.1',
@@ -11,20 +9,27 @@ REDIS_CON = {
     "PASS" : ''
 }
 
+# ---------------------------------------------------------
 # rabbit settings
-''' location '''
+''' set rabbit connection '''
 RABBIT_CON = {
     "LOCATION" : "172.16.12.17",
     "PORT" : 5672
 }
 
-''' '''
+''' set execution queue '''
 EX_Q = 'ex'
 
-''' '''
+''' set ackowledge queue '''
 ACK_Q = 'ack'
 
-
+# ---------------------------------------------------------
 # dags
-''' dag folder '''
+''' set dag folder '''
 DAG_FOLDER = 'dags'
+
+
+# ---------------------------------------------------------
+# task setting
+''' set task retries on faliure '''
+TASK_RETRY_COUNT = 3
