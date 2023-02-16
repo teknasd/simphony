6bec55004bf88136703fff0b79ec398efeb717c2
sanket <sanket.deshpande@caring-research.com>
2023-02-16 13:25:32 +0530

    error handling in state manager

--- state_manager.py
+++ state_manager.py
@@ -6,11 +6,19 @@ r = redis.Redis(
     password='')
 
 
+
 def push(key, val):
-    if val is None:
-        val = json.dumps({})
-    r.set(key, json.dumps(val))
+    try:
+        if val is None:
+            val = json.dumps({})
+        r.set(key, json.dumps(val))
+    except Exception:
+        print("State not pushed, incompatible data")
 
 
 def pull(key):
-    return {} if r.get(key) == b'' else json.loads(r.get(key))
+    try:
+        return {} if r.get(key) == b'' else json.loads(r.get(key))
+    except Exception:
+        print("State did not pulled, incompatible key")
+        return {}
