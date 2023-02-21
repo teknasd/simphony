370a01b193955581b2a3adeb5cc7633932429521
sanket <sanket.deshpande@caring-research.com>
2023-02-22 02:33:00 +0530

    api layer

---
+++ api.py
@@ -0,0 +1,40 @@
+import json
+from rabi import Rabi    
+import threading
+from pprint import pprint
+import config
+from controller import Controller
+from fastapi import FastAPI,Form
+import uvicorn
+from pydantic import BaseModel
+
+''' this is the main thread '''
+# C = Controller(filepaths=["funcs"])
+# C = Controller()
+
+app = FastAPI(
+    title="Simphony"
+)
+
+''' ------------------------'''
+
+class RunModel(BaseModel):
+    dag: str = None
+    context: str = None
+
+@app.post("/run")
+async def run_dag(dag: str = Form(...),context: str = Form(...)):
+    try:
+        print("paylload:",dag,context)
+        # C = Controller( [dag + ".py"])
+        # print(C.dag_store)
+        r = Rabi(q = config.CALL_Q)
+        task_obj = {"dag": dag}
+        r.push_to_q(json.dumps(task_obj))
+        r.close()
+        db_result = 'Success'
+        return {'status': 'Failed'} if db_result == "Failed" else {'status': 'Success'}
+    except Exception as e:
+        print(e)
+        return {'status': 'Failed'}
+
